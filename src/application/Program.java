f4f27fa84294a59af373ff3b00f1f8b7c006b049
Bruno Augusto <Brunoaug95@gmail.com>
2023-10-01 10:21:03 -0300

    add department class

---
+++ src/application/Program.java
@@ -0,0 +1,16 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+	
+
+		Department dp = new Department("Bruno",3);
+		
+		System.out.println(dp);
+		
+	}
+
+}
