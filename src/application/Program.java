d350caed467f33bb4266b7907d30fedf2f8fa7e6
Bruno Augusto <Brunoaug95@gmail.com>
2023-10-01 11:04:07 -0300

    add seller class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,24 @@
 package application;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-	
 
-		Department dp = new Department("Bruno",3);
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 		
+		Department dp = new Department("Bruno", 3);
+		Seller seller = new Seller(10, "pedro", "pedro@gmail.com", LocalDate.parse("07/06/1995",fmt), 3000.00, dp);
+		
+		
+
 		System.out.println(dp);
-		
+		System.out.println(seller);
 	}
 
 }
